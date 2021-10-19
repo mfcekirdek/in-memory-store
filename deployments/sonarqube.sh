256a1fe598ae4cf68950318f5ab44178f34805e9
mfcekirdek <mfcekirdek94@gmail.com>
2021-10-19 09:34:06 +0300

    add sonarqube configs and code-analysis pipeline stage.

---
+++ deployments/sonarqube.sh
@@ -0,0 +1,4 @@
+helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
+helm repo update
+kubectl create namespace sonarqube
+helm upgrade --install -n sonarqube sonarqube sonarqube/sonarqube
