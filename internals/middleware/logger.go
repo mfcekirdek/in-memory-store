1e272f5de6cb486c9b870f134baa4744760460fe
mfcekirdek <mfcekirdek94@gmail.com>
2021-10-20 23:20:00 +0300

    add response status codes to logs.

--- internals/middleware/logger.go
+++ internals/middleware/logger.go
@@ -10,10 +10,28 @@ type LoggerMiddleware struct {
 }
 
 func (l *LoggerMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	l.handler.ServeHTTP(w, r)
-	log.Printf("%s %s", r.Method, r.URL.Path)
+	lrw := newLoggingResponseWriter(w)
+	l.handler.ServeHTTP(lrw, r)
+	statusCode := lrw.statusCode
+	log.Printf("%s %s [%d] - %s", r.Method, r.URL.Path, statusCode, http.StatusText(statusCode))
 }
 
 func NewLoggerMiddleware(handlerToWrap http.Handler) *LoggerMiddleware {
-	return &LoggerMiddleware{handlerToWrap}
+	return &LoggerMiddleware{handler: handlerToWrap}
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func newLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	// WriteHeader(int) is not called if our response implicitly returns 200 OK, so
+	// we default to that status code.
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
 }
