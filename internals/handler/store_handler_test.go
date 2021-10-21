99d5051150436b7d1a55e4dbeb271617a79b108b
mfcekirdek <mfcekirdek94@gmail.com>
2021-10-22 02:22:13 +0300

    add tests for flush endpoint

--- internals/handler/store_handler_test.go
+++ internals/handler/store_handler_test.go
@@ -4,8 +4,12 @@ package handler
 
 import (
 	"github.com/golang/mock/gomock"
+	"gitlab.com/mfcekirdek/in-memory-store/internals/model"
 	"gitlab.com/mfcekirdek/in-memory-store/internals/service"
 	"gitlab.com/mfcekirdek/in-memory-store/mocks"
+	"gitlab.com/mfcekirdek/in-memory-store/test"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 )
@@ -33,58 +37,59 @@ func TestNewStoreHandler(t *testing.T) {
 	}
 }
 
-//func Test_storeHandler_Flush(t *testing.T) {
-//	mockController := gomock.NewController(t)
-//	mockService := mocks.NewMockStoreService(mockController)
-//	mockService.
-//		EXPECT().
-//		Flush().
-//		Return(map[string]string{}).
-//		Times(1)
-//
-//	w, r := test.CreateHTTPReq(http.MethodDelete, "/api/v1/store", nil)
-//
-//	type fields struct {
-//		service service.StoreService
-//	}
-//	type args struct {
-//		w *httptest.ResponseRecorder
-//		r *http.Request
-//	}
-//	tests := []struct {
-//		name   string
-//		fields fields
-//		args   args
-//		want   *model.BaseResponse
-//	}{
-//		{"[200] - flush all store data", fields{service: mockService}, args{
-//			w: w,
-//			r: r,
-//		}, &model.BaseResponse{
-//			Data:        map[string]interface{}{},
-//			Description: "all items deleted",
-//		}},
-//		{"[405] - StatusMethodNotAllowed", fields{service: mockService}, args{
-//			w: w,
-//			r: r,
-//		}, &model.BaseResponse{
-//			Data:        map[string]interface{}{},
-//			Description: "method not allowed",
-//		}},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			s := &storeHandler{
-//				service: tt.fields.service,
-//			}
-//
-//			s.Flush(tt.args.w, tt.args.r)
-//			if got := test.ParseBody(w.Body.Bytes()); !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("NewStoreService() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
+func Test_storeHandler_Flush(t *testing.T) {
+	mockController := gomock.NewController(t)
+	mockService := mocks.NewMockStoreService(mockController)
+	mockService.
+		EXPECT().
+		Flush().
+		Return(map[string]string{}).
+		Times(1)
+
+	w1, r1 := test.CreateHTTPReq(http.MethodDelete, "/api/v1/store", nil)
+	w2, r2 := test.CreateHTTPReq(http.MethodPost, "/api/v1/store", nil)
+
+	type fields struct {
+		service service.StoreService
+	}
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *model.BaseResponse
+	}{
+		{"[200] - flush all store data", fields{service: mockService}, args{
+			w: w1,
+			r: r1,
+		}, &model.BaseResponse{
+			Data:        map[string]interface{}{},
+			Description: "all items deleted",
+		}},
+		{"[405] - StatusMethodNotAllowed", fields{}, args{
+			w: w2,
+			r: r2,
+		}, &model.BaseResponse{
+			Data:        nil,
+			Description: "method not allowed",
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &storeHandler{
+				service: tt.fields.service,
+			}
+
+			s.Flush(tt.args.w, tt.args.r)
+			if got := test.ParseBody(tt.args.w.Body.Bytes()); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewStoreService() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 //func Test_storeHandler_ServeHTTP(t *testing.T) {
 //	type fields struct {
