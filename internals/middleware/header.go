283a2fee0f781735ad2ea695cac2cc231291ae60
mfcekirdek <mfcekirdek94@gmail.com>
2021-10-21 04:18:33 +0300

    add headers to allow cors

--- internals/middleware/header.go
+++ internals/middleware/header.go
@@ -9,12 +9,12 @@ type HeaderMiddleware struct {
 }
 
 func (l *HeaderMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 	if r.Method == "OPTIONS" {
-		w.WriteHeader(http.StatusOK)
 		return
 	}
 
